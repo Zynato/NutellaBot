49a245b72cc591c0f4a6fabdb3edbc48b2d56685
Zynato <zynato@mail.com>
2018-02-15 17:33:57 -0500

    Add Find method to GuildMappingCollection

--- src/GuildMappingCollection.cs
+++ src/GuildMappingCollection.cs
@@ -1,10 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace DiscordBot
 {
     public class GuildMappingCollection : List<GuildMapping>
     {
+        public GuildMapping Find(ulong guildId) {
+            return this.Where(x => x.Id == guildId).FirstOrDefault();
+        }
     }
 }
