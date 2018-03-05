78c0dbf66bcb9b6e6589db4b5fd7ef15c6341d06
Zynato <zynato@mail.com>
2018-03-04 21:11:06 -0500

    Add RequireGuildOwner precondition

--- src/Preconditions/RequireGuildAttribute.cs
+++ src/Preconditions/RequireGuildAttribute.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 using Microsoft.Extensions.DependencyInjection;
 using System.Linq;
 
-namespace DiscordBot
+namespace DiscordBot.Preconditions
 {
     public class RequireGuildAttribute : PreconditionAttribute
     {
---
+++ src/Preconditions/RequireGuildOwner.cs
@@ -0,0 +1,19 @@
+﻿using Discord.Commands;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiscordBot.Preconditions
+{
+    public class RequireGuildOwner : PreconditionAttribute
+    {
+        public override Task<PreconditionResult> CheckPermissions(ICommandContext context, CommandInfo command, IServiceProvider services) {
+            if (context.Guild.OwnerId == context.User.Id) {
+                return Task.FromResult(PreconditionResult.FromSuccess());
+            } else {
+                return Task.FromResult(PreconditionResult.FromError("You must be the owner of the server to use this command."));
+            }
+        }
+    }
+}
